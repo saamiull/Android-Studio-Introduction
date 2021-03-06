f57755d7bdf56ef424d4346aa68ca7dd15f958bd
sami2905 <bsef18a028@pucit.edu.pk>
2021-03-06 18:35:34 +0500

    Multiple Screens Task

---
+++ Test App/app/src/main/java/org/samiulhaq/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package org.samiulhaq.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: It is testing one.
+        setContentView(R.layout.activity_main);
+        String name = "Welcome to PUCIT";
+        // it will be a comment.
+        // TODO: it is testing two.
+    }
+}
